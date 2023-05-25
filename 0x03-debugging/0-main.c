8d004b29ecc54aed4f699b7d412e135e09d32cbc
Yamahdavies <Yamahdavies653@gmail.com>
2023-05-25 06:54:37 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,12 +3,13 @@
  * main - A function that prints positive or  negative numbers
  * Return:0 for success
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
+
 	return (0);
 }
