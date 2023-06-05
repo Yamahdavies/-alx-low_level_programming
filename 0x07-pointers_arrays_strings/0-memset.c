e455bdb814a18a9f1bd7921338b815905b0a5f7e
Yamahdavies <Yamahdavies653@gmail.com>
2023-06-05 08:21:17 -0700

    Advanced Task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
+ * _memset - a  filled block of memory with a specific value
+ * @s:  address of memory to be filled
+ * @b: the correct  value
  * @n: number of bytes to be changed
  *
  * Return: changed array with new value for n bytes
